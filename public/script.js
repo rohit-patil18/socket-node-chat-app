ce525a2d1ad254a433f5cc75a474185aee7e55bb
rohit-patil18 <rohit.patil1@somaiya.edu>
2023-08-24 23:33:00 +0530

    emoji mapping fix

--- public/script.js
+++ public/script.js
@@ -3,41 +3,37 @@ $(function () {
   const $messages = $("#messages");
   const $form = $("#form");
   const $input = $("#input");
-  const $username = $("#username"); // Add this line to get the username input field
+  const $username = $("#username");
+
+  const emojiMap = {
+    react: "⚛️",
+    woah: "😲",
+    hey: "👋",
+    lol: "😂",
+    like: "🤍",
+    congratulations: "🎉",
+  };
 
   $form.submit(function () {
-    const username = $username.val().trim(); // Get the username
-    const message = $input.val().trim();
+      const username = $username.val().trim();
+      let message = $input.val().trim(); // Get the user's message
 
-    if (username !== "" && message !== "") {
-      socket.emit("chat message", { username, message }); // Send username and message
-      $input.val("");
-    }
-    return false;
+      // Replace message words with emojis based on the emojiMap
+      message = message.replace(/\b\w+\b/g, match => {
+          return emojiMap[match.toLowerCase()] || match;
+      });
+
+      if (username !== "" && message !== "") {
+          socket.emit("chat message", { username, message });
+          $input.val("");
+      }
+      return false;
   });
 
   socket.on("chat message", function (data) {
-    const { username, message } = data; // Extract username and message from received data
-
-    const emojiMap = {
-      react: "⚛️",
-      woah: "😲",
-      hey: "👋",
-      lol: "😂",
-      like: "🤍",
-      congratulations: "🎉",
-    };
-
-    const newMessage = message.split(" ").map(item => {
-      for (const key in emojiMap) {
-        if (key.toLowerCase() === item.toLowerCase()) {
-          return emojiMap[key];
-        }
-        return item;
-      }
-    })
-
-    $messages.append($("<div>").html(`<span class="username">${username}:</span> ${newMessage}`).addClass("message"));
-    $messages.scrollTop($messages[0].scrollHeight);
+      const { username, message } = data;
+      $messages.append($("<div>").html(`<span class="username">${username}:</span> ${message}`).addClass("message"));
+      $messages.scrollTop($messages[0].scrollHeight);
   });
 });
+
